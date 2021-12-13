51ff542df09001b7475418949716daac154802a8
plagocastro <plagocastro@danielcastelao.org>
2021-12-13 09:30:17 +0100

    Subo main del Ej1 explicaión visual del onResume

---
+++ app/src/main/java/com/dam2/examenpmdm1/Ejercicio1.kt
@@ -0,0 +1,11 @@
+package com.dam2.examenpmdm1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ejercicio1 : AppCompatActivity() {
+    fun onResume(savedInstanceState: Bundle?) {
+        super.onResume()
+        setContentView(R.layout.activity_main)
+    }
+}
